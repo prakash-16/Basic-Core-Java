3abfce413dcdd56987b2cf8aca0c12fbd2755492
Prakash-16 <goudprakash58@gmail.com>
2021-08-10 20:11:48 +0530

    [Prakash] - Added Even/Odd number program

--- src/com/bridgelabz/BCJ/BasicCoreJava.java
+++ src/com/bridgelabz/BCJ/BasicCoreJava.java
@@ -3,17 +3,17 @@ import java.util.*;
 public class BasicCoreJava {
 	
 	public static void main(String[] args) {
-		int a,b,c;
+		int N;
 		Scanner sc = new Scanner(System.in);
 		System.out.println(" Enter the value of a :- ");
-		a = sc.nextInt();
-		System.out.println(" Enter the value of b :- ");
-		b = sc.nextInt();
-		System.out.println("The number before swapping "+ a +b);
-		c=a;
-		a=b;
-		b=c;
-		System.out.println("The number before swapping "+ a +b);
+		N = sc.nextInt();
+		if(N % 2 == 0) {
+			System.out.println("The number is Even");
+		}
+		else {
+			System.out.println("The number is Odd");
+		}
+		
 				
 		
     	
