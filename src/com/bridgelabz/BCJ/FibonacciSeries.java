bbb9199175791a38807b2f1dd45842b4e2e62753
Prakash-16 <goudprakash58@gmail.com>
2021-08-11 17:40:32 +0530

    Added Fibonacci Series Feature

---
+++ src/com/bridgelabz/BCJ/FibonacciSeries.java
@@ -0,0 +1,26 @@
+package com.bridgelabz.BCJ;
+
+import java.util.Scanner;
+
+public class FibonacciSeries {
+	
+	public static final int first_no = 0, second_no =1;
+	public static void main(String[] args) {
+		int N,i,sum;
+		Scanner sc = new Scanner(System.in);
+		System.out.println(" Enter the number for series :- ");
+		N = sc.nextInt();
+		int fib_series[] = new int[N];
+		fib_series[0] = first_no;
+		fib_series[1] = second_no;
+		for(i=2;i<N;i++) {
+			sum = fib_series[i-1] + fib_series[i-2];
+			fib_series[i] = sum; 
+		}
+		System.out.println("the fibonacci series is :- ");
+		for(i=0;i<N;i++) {
+			System.out.println(fib_series[i]);
+		}
+	}
+
+}
